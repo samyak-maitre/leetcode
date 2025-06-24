0f8db1ed80036b331f05dc63f8e846e9c2b9563c
samyak <samyakcodes@gmail.com>
2025-06-24 09:00:28 +0530

    88. Merge Sorted Array

--- 88.java
+++ 88.java
@@ -10,3 +10,23 @@ import java.util.*;
 //     }
 // }
 
+class Solution{
+    public void merger(int[] nums1, int m, int nums2[], int n){
+        int i = m-1;
+        int j = n-1;
+        int k = m+n -1;
+
+        //merge in reverse order
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while(j >= 0){
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
