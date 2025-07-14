9d3fd5363d8b05de6660744a1fb0fc4fc28df8f2
samyak <samyakcodes@gmail.com>
2025-07-14 22:28:50 +0530

    Longest Palindromic Substring

--- _5.java
+++ _5.java
@@ -19,6 +19,11 @@ class Solution{
     }
 
     private int expandAroundCenter(String s, int left, int right){
-        while(left >= 0 && right < s.length())
+        while(left >= 0 && right < s.length() && s.charAt(left)==s.charAt(right)){
+            left++;
+            right--;
+        }
+
+        return right-left+1;
     }
 }
